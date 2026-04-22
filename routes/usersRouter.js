3d8aa787471c35a531c15cc6f20f7d55bf585c1f
AdrishikharChodhury <amiadrishikhar@gmail.com>
2026-04-22 16:25:52 +0530

    Added Explore Page and features

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const dbgr = require("debug")("development: user");
+const { isLoggedIn } = require("../middlewares/loginVerify");
+const { generateToken } = require("../utils/generateToken");
+const userModel = require("../models/user-model");
 
 router.get("/dashboard", isLoggedIn, async (req, res) => {
   try {
